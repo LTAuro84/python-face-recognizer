94419c79871a5225ef0b2a47bacbafd30d95900f
Auro <sarrm20@icloud.com>
2025-04-13 00:14:41 -0400

    initial commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import time
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 cap = cv2.VideoCapture(1)
 
@@ -24,16 +25,29 @@ while True:
         print("Error when capturing image from the camera")
         break
 
-    img = cv2.flip(img, 1)
+    img = cv2.flip(img, 1) #mirror the image
     
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+    #Detect faces
+    faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.2, minNeighbors=5)
 
+    #Draw rectangle around the face
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
 
-    cv2.imshow('img', img)
+        #Extract the face ROI from the grayscale frame
+        face_roi = gray_frame[y:y+h, x:x+w]
+
+        #Detect smiles within the face ROI
+        smiles = smile_cascade.detectMultiScale(face_roi, scaleFactor=1.7, minNeighbors=20)
+
+        if len(smiles) > 0:
+            cv2.putText(img, 'Smiling', (x, y+h+40), fontScale=3, fontFace=cv2.FONT_HERSHEY_PLAIN, color=(255, 255, 255))
+
+    
+
+    cv2.imshow('Real-time Face and Smile Detection', img)
     cv2.waitKey(1)
 
     k = cv2.waitKey(30) & 0xff
